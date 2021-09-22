f807c93fd28304ed880c13e1e97dc33df2e155e5
John McBride <jmcbride@vmware.com>
2021-09-22 11:37:12 -0600

    Initial commit
    Ability to install TCE using Homebrew
    
    Signed-off-by: John McBride <jmcbride@vmware.com>

--- README.md
+++ README.md
@@ -1,29 +1,11 @@
-
 # homebrew-tanzu
+Homebrew tap for [Tanzu Community Edition](https://github.com/vmware-tanzu/community-edition)
 
-## Overview
+Usage
 
-## Try it out
+```shell
+brew tap vmware-tanzu/tanzu
+brew install tce
+```
 
-### Prerequisites
-
-* Prereq 1
-* Prereq 2
-* Prereq 3
-
-### Build & Run
-
-1. Step 1
-2. Step 2
-3. Step 3
-
-## Documentation
-
-## Contributing
-
-The homebrew-tanzu project team welcomes contributions from the community. Before you start working with homebrew-tanzu, please
-read our [Developer Certificate of Origin](https://cla.vmware.com/dco). All contributions to this repository must be
-signed as described on that page. Your signature certifies that you wrote the patch or have the right to pass it on
-as an open-source patch. For more detailed information, refer to [CONTRIBUTING.md](CONTRIBUTING.md).
-
-## License
+> Be sure to run the `install.sh` script referenced in the post-install steps.
---
+++ tce.rb
@@ -0,0 +1,172 @@
+class Tce < Formula
+  desc "Tanzu Community Edition"
+  homepage "https://github.com/vmware-tanzu/community-edition"
+  version "v0.8.0"
+  head "https://github.com/vmware-tanzu/community-edition.git"
+
+  if OS.mac?
+    url "https://github.com/vmware-tanzu/community-edition/releases/download/#{version}/tce-darwin-amd64-#{version}.tar.gz"
+    sha256 "e8b2087b01aad073af4e0b4e534f2ac9fe1e9b8cbcda9c52514b8c3197da272c"
+  elsif OS.linux?
+    url "https://github.com/vmware-tanzu/community-edition/releases/download/#{version}/tce-linux-amd64-#{version}.tar.gz"
+    sha256 "b7a2177b9bb0195fd4006c84536241e0e2045a6039346b8a1c6026e8c0d58091"
+  end
+
+  depends_on :arch => :x86_64
+
+  def install
+    bin.install "bin/tanzu"
+    libexec.install Dir["bin/tanzu-plugin-*"]
+
+    File.write("install.sh", brew_installer_script)
+    File.chmod(0755, "install.sh")
+    libexec.install "install.sh"
+
+    File.write("uninstall.sh", brew_uninstall_script)
+    File.chmod(0755, "uninstall.sh")
+    libexec.install "uninstall.sh"
+  end
+
+  def post_install
+    ohai "Thanks for installing Tanzu Community Edition!"
+    ohai "The Tanzu CLI has been installed on your system"
+    ohai "\n"
+    ohai "******************************************************************************"
+    ohai "* To initialize all plugins required by TCE, an additional step is required."
+    ohai "* To complete the installation, please run the following shell script:"
+    ohai "*"
+    ohai "* #{libexec}/install.sh"
+    ohai "*"
+    ohai "******************************************************************************"
+    ohai "\n"
+    ohai "To cleanup and remove TCE from your system, run the following command:"
+    ohai "#{libexec}/uninstall.sh"
+
+  end
+
+  # Homebrew requires tests.
+  test do
+    assert_match("ceaa474", shell_output("#{bin}/tanzu version", 2))
+  end
+
+  def brew_installer_script
+    <<-EOF
+#!/bin/bash
+
+# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+ALLOW_INSTALL_AS_ROOT="${ALLOW_INSTALL_AS_ROOT:-""}"
+if [[ "$EUID" -eq 0 && "${ALLOW_INSTALL_AS_ROOT}" != "true" ]]; then
+  echo "Do not run this script as root"
+  exit 1
+fi
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "MY_DIR: ${MY_DIR}"
+BUILD_OS=$(uname 2>/dev/null || echo Unknown)
+
+case "${BUILD_OS}" in
+  Linux)
+    XDG_DATA_HOME="${HOME}/.local/share"
+    ;;
+  Darwin)
+    XDG_DATA_HOME="${HOME}/Library/Application Support"
+    ;;
+  *)
+    echo "${BUILD_OS} is unsupported"
+    exit 1
+    ;;
+esac
+echo "${XDG_DATA_HOME}"
+
+# install all plugins present in the bundle
+mkdir -p "${XDG_DATA_HOME}/tanzu-cli"
+for plugin in "${MY_DIR}"/tanzu-plugin*; do
+  install "${plugin}" "${XDG_DATA_HOME}/tanzu-cli"
+done
+
+# copy the uninstall script to tanzu-cli directory
+mkdir -p "${XDG_DATA_HOME}/tce"
+install "${MY_DIR}/uninstall.sh" "${XDG_DATA_HOME}/tce"
+
+# if plugin cache pre-exists, remove it so new plugins are detected
+TANZU_PLUGIN_CACHE="${HOME}/.cache/tanzu/catalog.yaml"
+if [[ -n "${TANZU_PLUGIN_CACHE}" ]]; then
+  echo "Removing old plugin cache from ${TANZU_PLUGIN_CACHE}"
+  rm -f "${TANZU_PLUGIN_CACHE}" > /dev/null
+fi
+
+# Make a backup of Kubernetes configs
+set +o errexit
+echo "Making a backup of your Kuberentes config files int /tmp"
+tar cf /tmp/`date "+%Y%m%d%H%M"`-kubernetes-configs.tar ~/.kube ~/.kube-tkg ~/.tanzu ~/.config/tanzu 2>/dev/null
+set -o errexit
+
+# explicit init of tanzu cli and add tce repo
+TANZU_CLI_NO_INIT=true tanzu init
+#TCE_REPO="$(tanzu plugin repo list | grep tce)"
+# if [[ -z "${TCE_REPO}"  ]]; then
+#   tanzu plugin repo add --name tce --gcp-bucket-name tce-cli-plugins --gcp-root-path artifacts
+# fi
+
+# TCE_REPO="$(tanzu plugin repo list | grep core-admin)"
+# if [[ -z "${TCE_REPO}"  ]]; then
+#   tanzu plugin repo add --name core-admin --gcp-bucket-name tce-tanzu-cli-admin-plugins --gcp-root-path artifacts-admin
+# fi
+
+echo "Installation complete!"
+EOF
+  end
+
+  def brew_uninstall_script
+    <<-EOF
+#!/bin/bash
+
+# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+BUILD_OS=$(uname 2>/dev/null || echo Unknown)
+
+case "${BUILD_OS}" in
+  Linux)
+    XDG_DATA_HOME="${HOME}/.local/share"
+    ;;
+  Darwin)
+    XDG_DATA_HOME="${HOME}/Library/Application Support"
+    ;;
+  *)
+    echo "${BUILD_OS} is unsupported"
+    exit 1
+    ;;
+esac
+echo "${XDG_DATA_HOME}"
+
+rm -rf "${XDG_DATA_HOME}/tanzu-cli" \
+  "${XDG_DATA_HOME}/tce" \
+  ${HOME}/.cache/tanzu/catalog.yaml \
+  ${HOME}/.config/tanzu/config.yaml \
+  ${HOME}/.config/tanzu/tkg/bom \
+  ${HOME}/.config/tanzu/tkg/providers \
+  ${HOME}/.config/tanzu/tkg/.tanzu.lock \
+  ${HOME}/.config/tanzu/tkg/compatibility/tkg-compatibility.yaml
+
+echo "Cleanup complete!"
+echo
+echo "Removing the Tanzu CLI..."
+echo
+brew uninstall tce
+EOF
+  end
+end
