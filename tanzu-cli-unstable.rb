34b05d615fc5c8eef88ed8ae2998c64e15bc5f16
Marc Khouzam <marc.khouzam@broadcom.com>
2024-01-08 22:13:07 -0500

    Add 1.2.0-alpha.0 pre-release (#52)
    Signed-off-by: Marc Khouzam <marc.khouzam@broadcom.com>

--- tanzu-cli-unstable.rb
+++ tanzu-cli-unstable.rb
@@ -4,14 +4,14 @@
 class TanzuCliUnstable < Formula
   desc "The core Tanzu command-line tool (unstable builds)"
   homepage "https://github.com/vmware-tanzu/tanzu-cli"
-  version "1.1.0-rc.0"
+  version "1.2.0-alpha.0"
   head "https://github.com/vmware-tanzu/tanzu-cli.git", branch: "main"
 
   checksums = {
-    "darwin-amd64" => "81b6330dc8481c1aa51d4fc76aabe3ae132bd76d207a531d4481c8578ad9d5d7",
-    "darwin-arm64" => "d0d18be55efe25f3407bdca51dc7ba29db76eb8864f00404e23c449a69cf06cd",
-    "linux-amd64"  => "e6c83fde072512435a2910fa297bdf4f9906950044ccc12faa91eadcf376eebe",
-    "linux-arm64"  => "a72e01baf0f8173b92141b266e28f21461bd9641a3fd44e1acc31b125e9ee8fc",
+    "darwin-amd64" => "f1c310adedbc1339df18dce33c71856a030c76d921fd09a7734b50b2cd9f3f3b",
+    "darwin-arm64" => "db2ae9a48bd78a956442d3765fd754fe415246893c42d831e9e06095d7eaddbe",
+    "linux-amd64"  => "d06c3fc0cda7d1e353f3f6d52e849e43632de903428fc9d8acbf4d0646d3d526",
+    "linux-arm64"  => "e1130289ae118cc0b465375da5c463f34c69d986a8d9e711a64e5643a9ca1090",
   }
 
   $arch = "arm64"
